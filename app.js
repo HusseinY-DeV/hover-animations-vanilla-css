b6cdd40768fb52c6ecb2a5b1c2ae1b2333494dd0
6memes9 <userahmadassaad6@gmail.com>
2021-02-10 17:58:23 +0200

    added svg animation

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 let tl = gsap.timeline();
 
-tl.to('.slider p',{duration:2,opacity:1})
-.to('.slider',{duration:1,y:"-100%"},"+=2")
+tl.to('.slider p', { duration: 2, opacity: 1 })
+    .to('.slider', { duration: 1, y: "-100%" }, "+=2")
+
+
+let svg = document.querySelectorAll("#design path");
+
+for (let i = 0; i < svg.length; i++) {
+    console.log(svg[i].getTotalLength());
+}
